5760581769501c1005dfc5b66ac6fd064de1101f
esdx245 <esdx245@khu.ac.kr>
2024-02-08 02:07:31 +0900

    show text & longTap & Tap & Draggable

--- lib/screens/edit_image_screen.dart
+++ lib/screens/edit_image_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:clone_image_edit/widgets/edit_image_viewmodel.dart';
+import 'package:clone_image_edit/widgets/image_text.dart';
 import 'package:flutter/material.dart';
 
 class EditImageScreen extends StatefulWidget {
@@ -15,10 +16,51 @@ class _EditImageScreenState extends EditImageViewModel {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.file(File(widget.slectedImage)),
+      body: SafeArea(
+          child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.3,
+        child: Stack(
+          children: [
+            _selectedImage,
+            for (int i = 0; i < texts.length; i++)
+              Positioned(
+                left: texts[i].left,
+                top: texts[i].top,
+                child: GestureDetector(
+                    onLongPress: () {
+                      print('long press detected');
+                    },
+                    onTap: () {
+                      print('tap detected');
+                    },
+                    child: Draggable(
+                      feedback: ImageText(textInfo: texts[i]),
+                      child: ImageText(textInfo: texts[i]),
+                      onDragEnd: (drag) {
+                        final renderBox =
+                            context.findRenderObject() as RenderBox;
+                        Offset off = renderBox.globalToLocal(drag.offset);
+                        setState(() {
+                          texts[i].top = off.dy;
+                          texts[i].left = off.dx;
+                        });
+                      },
+                    )),
+              )
+          ],
+        ),
+      )),
       floatingActionButton: _addNewTextFab,
     );
   }
+
+  Widget get _selectedImage => Center(
+        child: Image.file(
+          File(widget.slectedImage),
+          fit: BoxFit.fill,
+          width: MediaQuery.of(context).size.width,
+        ),
+      );
 
   Widget get _addNewTextFab => FloatingActionButton(
         onPressed: () => addNewDialog(context),
