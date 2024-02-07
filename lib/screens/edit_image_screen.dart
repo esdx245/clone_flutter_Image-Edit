af3c6a1a4a83236bc79ce4e3363fb4896961a15d
esdx245 <esdx245@khu.ac.kr>
2024-02-08 01:33:40 +0900

    add editButton & navigate back & add Text

--- lib/screens/edit_image_screen.dart
+++ lib/screens/edit_image_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:clone_image_edit/widgets/edit_image_viewmodel.dart';
 import 'package:flutter/material.dart';
 
 class EditImageScreen extends StatefulWidget {
@@ -10,11 +11,22 @@ class EditImageScreen extends StatefulWidget {
   State<EditImageScreen> createState() => _EditImageScreenState();
 }
 
-class _EditImageScreenState extends State<EditImageScreen> {
+class _EditImageScreenState extends EditImageViewModel {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Image.file(File(widget.slectedImage)),
+      floatingActionButton: _addNewTextFab,
     );
   }
+
+  Widget get _addNewTextFab => FloatingActionButton(
+        onPressed: () => addNewDialog(context),
+        backgroundColor: Colors.white,
+        tooltip: 'Add New Text',
+        child: const Icon(
+          Icons.edit,
+          color: Colors.black,
+        ),
+      );
 }
---
+++ lib/widgets/edit_image_viewmodel.dart
@@ -0,0 +1,53 @@
+import 'package:clone_image_edit/models/text_info.dart';
+import 'package:clone_image_edit/screens/edit_image_screen.dart';
+import 'package:clone_image_edit/widgets/default_button.dart';
+import 'package:flutter/material.dart';
+
+abstract class EditImageViewModel extends State<EditImageScreen> {
+  TextEditingController textEditingController = TextEditingController();
+
+  List<TextInfo> texts = [];
+  addNewText(BuildContext context) {
+    setState(() {
+      texts.add(TextInfo(
+          text: textEditingController.text,
+          left: 0,
+          top: 0,
+          color: Colors.black,
+          fontWeight: FontWeight.normal,
+          fontStyle: FontStyle.normal,
+          fontSize: 20,
+          textAlign: TextAlign.left));
+      Navigator.of(context).pop();
+    });
+  }
+
+  addNewDialog(context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('Add New Text'),
+        content: TextField(
+          controller: textEditingController,
+          maxLines: 5,
+          decoration: const InputDecoration(
+              suffixIcon: Icon(Icons.edit),
+              filled: true,
+              hintText: 'Your Text Here..'),
+        ),
+        actions: <Widget>[
+          DefaultButton(
+              onPressed: () => Navigator.of(context).pop(),
+              color: Colors.white,
+              textColor: Colors.black,
+              child: const Text('Back')),
+          DefaultButton(
+              onPressed: () => addNewText(context),
+              color: Colors.red,
+              textColor: Colors.black,
+              child: const Text('Add Text'))
+        ],
+      ),
+    );
+  }
+}
